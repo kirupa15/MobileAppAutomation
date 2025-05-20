7032478fa20575921e90e66974ec7d4f2bc75bba
iinvsysqa <iinvsysqa@gmail.com>
2025-05-20 19:10:44 +0530

    pull'

--- src/test/java/wrappers/MobileAppWrappers.java
+++ src/test/java/wrappers/MobileAppWrappers.java
@@ -89,7 +89,6 @@ public class MobileAppWrappers extends GenericWrappers {
 //		applog.startLogProcess();
 		
 //		ABDconnection();
-		SwitchWiFi.switchwifi(loadProp("WIFINAME"), loadProp("WIFIPASSWORD"));
 	}
 		
 
